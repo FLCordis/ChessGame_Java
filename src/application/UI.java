16ed29b5029be90dd5eedcbf90019ec6f0b6e6eb
Flavio Cordis <flavio.cordis@hotmail.com>
2022-09-01 08:00:34 -0300

    Check logic added

--- src/application/UI.java
+++ src/application/UI.java
@@ -69,6 +69,9 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
 		System.out.println("Waiting for player " + chessMatch.getCurrentPlayer() + " to make a move.");
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
@@ -105,13 +108,13 @@ public class UI {
 		System.out.println("Captured pieces: ");
 		
 		System.out.print("White: ");
-		System.out.println(ANSI_WHITE);
-		System.out.println(Arrays.toString(white.toArray()));
+		System.out.print(ANSI_WHITE);
+		System.out.print(Arrays.toString(white.toArray()));
 		System.out.println(ANSI_RESET);
 		
 		System.out.print("Black: ");
-		System.out.println(ANSI_YELLOW);
-		System.out.println(Arrays.toString(black.toArray()));
+		System.out.print(ANSI_YELLOW);
+		System.out.print(Arrays.toString(black.toArray()));
 		System.out.println(ANSI_RESET);
 	}
 }
